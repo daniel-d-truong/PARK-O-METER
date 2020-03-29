f24212bb6bd45338265e24dc1f47ecda5ed92f3f
Trisa Leung <trisaleung1812@gmail.com>
2020-03-28 19:31:07 -0700

    basic first page

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,62 @@
 import React from 'react';
+import './App.css'
 
 class App extends React.Component {
+
+  handleSubmit = formSubmitEvent => {
+    formSubmitEvent.preventDefault();
+
+    alert("TESTING: submitted!");
+  };
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="main">
+        <style>
+          @import url('https://fonts.googleapis.com/css?family=Quicksand&display=swap');
+        </style>
+        <div className="titleSection">
+
+          <h1 className="title">
+            TITLE TITLE TITLE TITLE
+          </h1>
+
+          <h2 className="subtitle">
+            subtitle subtitle subtitle subtitle subtitle
+          </h2>
+
+          <form 
+            onSubmit={this.handleSubmit} 
+            className="form">
+
+            <input 
+              type="text" 
+              placeholder="starting point" 
+              className="location-input"
+              id="start-input"/>
+            <br />
+            <input 
+              type="text" 
+              placeholder="destination" 
+              className="location-input"
+              id="destination-input"/>
+
+          <button 
+            className="submit-button" 
+            type="submit">
+               let's go!
+            </button>
+
+          </form>
+
+
+        </div>
+        <div className="mapSection">
+          MAP
+        </div>
+        <footer className="footer">
+          hello
+        </footer>
       </div>
     );
   }
